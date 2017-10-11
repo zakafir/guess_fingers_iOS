f7f910b6b6e5efd65c5cd9aabf2c4f0ddfbd196b
zakafir <zak.afir@gmail.com>
2017-10-11 20:30:01 +0100

    Initial Commit

---
+++ guess_fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guess_fingers
+//
+//  Created by zakaria afir on 11/10/2017.
+//  Copyright © 2017 ZakariaAfir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
