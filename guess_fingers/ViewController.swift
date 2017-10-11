3984f03adb7b15ba900a2a5e72f7516aab1dd7de
zakafir <zak.afir@gmail.com>
2017-10-11 21:17:46 +0100

    last commit

--- guess_fingers/ViewController.swift
+++ guess_fingers/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberField: UITextField!
+    
+    @IBOutlet weak var numberGuessed: UILabel!
+    
+    
+    @IBAction func guessButton(_ sender: Any) {
+        let randomNumber = String(arc4random_uniform(6))
+        if numberField.text == randomNumber {
+            numberGuessed.text = "You're right!"
+        }else{
+            numberGuessed.text = "You're wrong! the right number is \(randomNumber)"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
